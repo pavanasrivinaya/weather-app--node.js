b367fe23a374f5530f13066fbed1a59d007d9a21
pavanasrivinaya <paripalepu@gmail.com>
2021-02-06 00:03:09 +0530

    forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find the location', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] +". It is currently " +body.current.temperature+ " degree out . It feels like " +body.current.feelslike+ "out there")
+            callback(undefined, body.current.weather_descriptions[0] +". It is currently " +body.current.temperature+ " degrees out . It feels like " +body.current.feelslike+ " degrees out there. The humidity is " +body.current.humidity + "%. ")
         }
     })
 }
