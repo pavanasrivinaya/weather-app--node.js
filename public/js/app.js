4c2a18d36320b11b62d74e711931c2863c300014
pavanasrivinaya <paripalepu@gmail.com>
2021-02-05 23:13:15 +0530

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side js file is loaded!!!")
-
 // SEarch form for the location
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
